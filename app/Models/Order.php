1fbd4fd0b5ee2852f03e0f81e135b130e544fdf4
Crescent Sambila <crescent.sambila@gmail.com>
2026-03-17 13:35:26 +0300

    Fix error

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -34,7 +34,7 @@ class Order extends Model
      */
     public function customer(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'customer_id');
+        return $this->belongsTo(Customer::class, 'customer_id');
     }
 
     /**
