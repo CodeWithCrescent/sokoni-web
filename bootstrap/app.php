a297abc5af1682c3408eb258439e30c0282be552
Crescent Sambila <crescent.sambila@gmail.com>
2026-03-17 12:42:01 +0300

    Registered middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\CheckRole;
+use App\Http\Middleware\CheckVendorOwnership;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,8 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-
-        // $middleware->append(CheckRole::class);
+        $middleware->alias([
+            'role' => CheckRole::class,
+            'vendor.ownership' => CheckVendorOwnership::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
